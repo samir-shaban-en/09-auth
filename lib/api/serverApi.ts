9129d092562561452a7b5b641940e53e4f67bdc6
Samir Shaban <samirshabanpsn@gmail.com>
2025-11-17 22:00:05 +0100

    fix

---
+++ lib/api/serverApi.ts
@@ -0,0 +1,15 @@
+import { cookies } from 'next/headers';
+import nextServer from './api';
+
+export const checkServerSession = async () => {
+  // Дістаємо поточні cookie
+  const cookieStore = await cookies();
+  const res = await nextServer.get('/auth/session', {
+    headers: {
+      // передаємо кукі далі
+      Cookie: cookieStore.toString(),
+    },
+  });
+  // Повертаємо повний респонс, щоб middleware мав доступ до нових cookie
+  return res;
+};
